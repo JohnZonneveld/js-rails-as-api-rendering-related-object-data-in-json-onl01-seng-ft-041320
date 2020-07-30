87604325a1d5205187c3c506c3aef7dc368c3667
John Zonneveld <jjczon@gmail.com>
2020-07-30 19:01:59 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,4 +3,9 @@ class SightingsController < ApplicationController
     sighting = Sighting.find_by(id: params[:id])
     render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
   end
+  
+  # def show
+  #   sighting = Sighting.find_by(id: params[:id])
+  #   render json: sighting, include: [:bird, :location]
+  # end
 end
